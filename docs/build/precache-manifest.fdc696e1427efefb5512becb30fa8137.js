bfb71f9747725b2e52860b610154b7fa7a36dcd2
saramarin23 <saramarin.econ@gmail.com>
2019-10-18 00:21:48 +0200

    add hover effect and docs folder

---
+++ docs/build/precache-manifest.fdc696e1427efefb5512becb30fa8137.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2fa9a47ffba40a34ead9e6001ced7c1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a011f25252c2bebd1855",
+    "url": "./static/css/main.0cd76c3e.chunk.css"
+  },
+  {
+    "revision": "22d89be5cf147c7b2d22",
+    "url": "./static/js/2.2e25121d.chunk.js"
+  },
+  {
+    "revision": "a011f25252c2bebd1855",
+    "url": "./static/js/main.e23bd0e4.chunk.js"
+  },
+  {
+    "revision": "877a035caeacfb77bd89",
+    "url": "./static/js/runtime-main.34392fff.js"
+  },
+  {
+    "revision": "397b89729ed9e38bb3e79d7144b54e8d",
+    "url": "./static/media/bgPokemon.397b8972.jpg"
+  }
+]);
